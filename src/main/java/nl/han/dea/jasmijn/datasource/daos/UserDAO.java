921683fcae7a35145a3345ba5c6a6a9caacc864e
JasmijnB97 <jasmijn.bartelds@hotmail.nl>
2018-10-26 15:11:05 +0200

    removed unused import and code

--- src/main/java/nl/han/dea/jasmijn/datasource/daos/UserDAO.java
+++ src/main/java/nl/han/dea/jasmijn/datasource/daos/UserDAO.java
@@ -1,6 +1,5 @@
 package nl.han.dea.jasmijn.datasource.daos;
 
-import nl.han.dea.jasmijn.dtos.LoginRequestDTO;
 import nl.han.dea.jasmijn.dtos.LoginResponseDTO;
 
 import java.sql.Connection;
@@ -13,12 +12,9 @@ import java.util.logging.Level;
 
 public class UserDAO extends DAO {
 
-//    private static final String GET_USER = "SELECT * FROM user  WHERE token = ?";
     private static final String GET_USER = "SELECT * FROM user  WHERE name = ? AND password = ?";
     private static final String UPDATE_TOKEN = "UPDATE user SET token = ? WHERE name = ? AND password = ?";
     private static final String GET_USER_ID = "SELECT id FROM user WHERE token = ?";
-    private static final String GET_TOKEN = "SELECT token FROM user  WHERE id = ?";
-
 
     public LoginResponseDTO getUser(String username, String password){
         LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
@@ -53,30 +49,6 @@ public class UserDAO extends DAO {
 
         updateQuery(UPDATE_TOKEN, bindings);
     }
-
-    public String getToken(int userId){
-        String token = "";
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet rs = null;
-        try {
-            connection = getDbConnection();
-            statement = connection.prepareStatement(GET_TOKEN);
-            statement.setInt(1, userId);
-            rs = statement.executeQuery();
-
-            while (rs.next()) {
-                token = rs.getString("token");
-            }
-
-        } catch (SQLException e){
-            LOGGER.log(Level.SEVERE, "Failed to get a token reason: " + e.getMessage());
-        } finally {
-            closeConnection(connection, statement, rs);
-        }
-        return token;
-    }
-
 
     public int getUserId(String token){
         int userId = -1;
